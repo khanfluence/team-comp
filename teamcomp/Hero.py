167b0080d678831839f7b5c1b032e306b864fc8a
khanfluence <khanfluence@gmail.com>
2018-04-26 09:50:18 -0700

    Align multi-line parameters

--- teamcomp/Hero.py
+++ teamcomp/Hero.py
@@ -11,8 +11,8 @@ class Hero:
     def fetch_stats(self):
         page = requests.get(f'https://www.dotabuff.com/heroes/{self.url}/counters',
                             headers={"user-agent": "Mozilla/5.0"})
-        data = re.findall(r'<td (?:class=".+?".+?)?data-value="(.+?)">', re.search(
-                r"<section><header>Matchups[\s\S]+</section>", page.text).group())
+        data = re.findall(r'<td (?:class=".+?".+?)?data-value="(.+?)">',
+                          re.search(r"<section><header>Matchups[\s\S]+</section>", page.text).group())
 
         for i in range(0, len(data), 4):
             self.stats[data[i]] = float(data[i + 1])
