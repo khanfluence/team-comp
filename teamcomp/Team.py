da7c18b51b424367848d7bd96cce23e5bf01a19d
khanfluence <khanfluence@gmail.com>
2019-02-16 16:08:53 -0800

    Don't store running averages (inaccurate); compute from scratch each time

--- teamcomp/Team.py
+++ teamcomp/Team.py
@@ -1,27 +1,37 @@
+from collections import defaultdict
 from Hero import Hero
 
 
 class Team:
     def __init__(self):
         self.heroes = list()
-        self.stats = dict()
+        self.stats = defaultdict(float)
 
     def add_hero(self, hero: Hero) -> bool:
-        if hero.name in self.heroes or len(self.heroes) == 5:
+        if hero in self.heroes or len(self.heroes) == 5:
             return False
 
-        self.heroes.append(hero.name)
-        for hero_name, stat in hero.stats.items():
-            self.stats[hero_name] = (self.stats[hero_name] + ((stat - self.stats[hero_name]) / len(self.heroes)))\
-                if hero_name in self.stats else stat
-
+        self.heroes.append(hero)
+        for hero in self.heroes:
+            self.stats.pop(hero.name, None)
+        self.calc_stats()
         return True
 
     def remove_hero(self, hero: Hero):
-        self.heroes.remove(hero.name)
-        for hero_name, stat in hero.stats.items():
-            self.stats[hero_name] = (self.stats[hero_name] * (len(self.heroes) + 1) - stat) / max(len(self.heroes), 1)
+        self.heroes.remove(hero)
+        if len(self.heroes) == 0:
+            self.reset()
+        else:
+            self.calc_stats()
+
+    def calc_stats(self):  # inefficient, but more correct than running averages
+        self.stats = defaultdict(float)
+        for hero_name in self.heroes[0].stats:
+            if hero_name not in [hero.name for hero in self.heroes]:
+                for hero in self.heroes:
+                    self.stats[hero_name] += hero.stats[hero_name]
+                self.stats[hero_name] /= len(self.heroes)
 
     def reset(self):
         self.heroes = list()
-        self.stats = dict()
+        self.stats = defaultdict(float)
